29214ffb68bbd11068f524a7ea0d57918b2dcd5e
vimalavinod8275 <75220042+vimalavinod8275@users.noreply.github.com>
2022-08-10 17:42:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+
+function preload() {
+    trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+    trex_collided = loadImage("trex_collided.png");
+    groundImage = loadImage("ground2.png")
+}
+
+
+function setup() {
+    createCanvas(600, 200);
+    //create a trex sprite
+    trex = createSprite(50,160,20,50);
+    trex.addAnimation("running", trex_running);
+    trex.scale = 0.5;
+    //create a ground sprite
+    ground = createSprite(200,180,400,20);
+    ground.addImage("ground",groundImage);
+    ground.x = ground.width /2;
+    ground.velocityX = -4;
+}
+
+
+function draw() {
+background(220);
+//jump when the space button is pressed
+if (keyDown("space")) {
+    trex.velocityY = -10;
+}
+
+//giving trex gravity
+trex.velocityY = trex.velocityY + 0.8;
+//to create an illusion of infinite ground
+if (ground.x < 0) {
+    ground.x = ground.width / 2;
+}
+//to stop trex going below the canvas/ground
+trex.collide(ground);
+drawSprites();
+}
